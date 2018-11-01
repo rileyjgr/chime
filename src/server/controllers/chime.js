4283dc7eaf00c1e9dbf2abcfc218cf2c61478847
Riley Griffin <rigriffin241@gmail.com>
2018-10-31 23:45:47 -0400

    added brain js and configuration, will turn config into its own file later

--- src/server/controllers/chime.js
+++ src/server/controllers/chime.js
@@ -1,12 +1,60 @@
-const botKit = require('botkit');
-const controller = botKit.anywhere(configuration);
-
+const brain = require('brain.js');
+const data = require('../Frames-dataset/frames.json');
 
 module.exports = {
-    bot: async(req, res, next)=> {
-        controller.hears('hello','direct_message', function(bot, message) {
-            bot.reply(message,'Hello yourself!');
-        })
+    chimer: async(res)=> {
+
+
+        // for now we will just import data sets.
+        const crossValidate = new brain.CrossValidate(brain.NeuralNetwork, networkOptions);
+
+        crossValidate.train(data, trainingOptions, k); //note k (or KFolds) is optional
+        const net = crossValidate.toNeuralNetwork({
+            activation: 'sigmoid',
+            hiddenLayers: [20],
+            learningRate: 0.6
+        });
+
+        net.fromJSON(data);
+
+        const options = {
+            iterations: 20000,
+            errorThresh: 0.005,
+            log: false,
+            logPeriod: 10,
+            learningRate: 0.3,
+            momentum: 0.1,
+            callback: null,
+            callbackPeriod: 10,
+            timeout: Infinity
+        };
+
+        //Train stream, seems better then async await.
+
+
+        const trainStream = new brain.TrainStream({
+            neuralNetwork: net,
+            floodCallback: () => {
+                flood(trainStream, data);
+            },
+            doneTrainingCallback: (stats) => {
+                // network finished.
+                console.log(stats);
+            }
+        });
+        const readInputs = (stream, data) => {
+            for (let i=0; i<data.length; i++) {
+                stream.write(data[i]);
+            }
+            stream.endInputs();
+        };
+
+        readInputs(trainStream, data);
+
+        //send it back
+
+        res.send('We done buddy');
+        next();
     }
 };
 
--- src/server/routes/routes.js
+++ src/server/routes/routes.js
@@ -2,12 +2,13 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
 const frames = require('../Frames-dataset/frames.json');
+const controllers = require('../controllers/controllers.js');
+
+const chime = require('../controllers/chime.js');
 
 module.exports ={
   html: async(app)=>{
-      app.get('/dataset', function(req, res){
-          res.send(frames)
-      });
+      app.get('/dataset', chime.chimer);
       app.use(express.static(`${__dirname}/../../dist`));
       app.use(express.static(`${__dirname}/../../dist/js/contact.js`));
       app.get('*', (req, res) => {
