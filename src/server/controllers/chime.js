473f85957019c567547a4d234df135524043380d
Riley Griffin <rigriffin241@gmail.com>
2018-11-09 21:26:40 -0500

    added signup logic

--- src/server/controllers/chime.js
+++ src/server/controllers/chime.js
@@ -1,22 +1,27 @@
 const brain = require('brain.js');
+const api = require('./controllers.js');
 
 
 module.exports = {
     chime: async(req, res)=>{
         console.log(req);
+        const action = req.body.result.action;
+        const request = req.body;
+        // not sure if this is correct or not
+        const user = req.body.result.name;
         // each action will be a case in a switch statement.
         // each action is an intent.
-        // we can add entities to each intent, to get data from the user such as their slack name.
-        // we can then store this stuff in a database (in our case mongodb).
-        if (req.body.result.action === 'hello') {
-            // we will do all of our api calls to controllers.js here.
 
-            // then await for our response.
-            await res.json({
-                speech: 'surprise',
-                displayText: 'Hi!',
-                source: 'hi'
-            });
+        switch(action){
+            case 'hello':
+                api.hello(request, res);
+                break;
+            case 'calender':
+               api.calender(request, res);
+               break;
+            case '':
+                break;
+
         }
     }
 };
--- src/server/controllers/controllers.js
+++ src/server/controllers/controllers.js
@@ -1,1 +1,54 @@
+const User = require('../models/user');
 
+module.exports = {
+    // we can add entities to each intent, to get data from the user such as their slack name.
+    // we can then store this stuff in a database (in our case mongodb).
+
+    hello: async(request,res)=>{
+
+        // example res of what we send back to user
+        await res.json({
+            speech: 'surprise',
+            displayText: 'Hi!',
+            source: 'hi'
+        });
+    },
+
+    calender: async(request, res)=>{
+        // Your code up here
+
+        await res.json({
+           // your response here
+        });
+    },
+
+    // logic for user sign up...
+
+    signUp: async(request, res)=>{
+        const user = request.body.name; // or whatever it is...
+        const founderUser = await User.findOne({user});
+        switch(founderUser){
+            case 'true':
+                res.json({
+                    speech: 'Try logging in',
+                    displayText: 'You already have an account.',
+                    source: 'user'
+                });
+                break;
+            case 'false':
+
+                const newUser = new User({name, title, company});
+
+                await newUser.save();
+
+                // not sure how we call their name here..
+                res.json({
+                    speech: 'Welcome %name%',
+                    displayText: 'Welcome %name%',
+                    source: 'user'
+                });
+                break;
+        }
+
+    }
+};
