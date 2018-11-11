4aef1dd8aef6337a838bb9ad523e298e23573100
Riley Griffin <rigriffin241@gmail.com>
2018-11-11 00:06:58 -0500

    getting so much closer, have it logging errors foreach request now

--- src/server/controllers/chime.js
+++ src/server/controllers/chime.js
@@ -1,17 +1,20 @@
 const brain = require('brain.js');
 const api = require('./controllers.js');
+const util = require('util')
 const { dialogflow, Image, Table, Carousel } = require('actions-on-google');
 
 module.exports = {
     chime: async(req, res)=>{
-        console.log(req);
-        const request = req.body.result;
-        const intent = req.body.arguments.rawText;
-
-        switch(intent){
-            case 'hello':
-                api.hello(request, res);
-        }
+        console.log(`post/${util.inspect(req,false,null)}`);
+        // const request = req.body.result;
+        // const intent = req.body.arguments.rawText;
+        //
+        // switch(intent){
+        //     case 'hello':
+        //         console.log(req);
+        //         api.hello(request, res);
+        // }
+        return res;
     }
 
 
