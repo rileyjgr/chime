13cf25926e419b21ee85439d9f380aa7d9b1035f
Riley Griffin <rigriffin241@gmail.com>
2018-11-12 19:12:08 -0500

    added a bunch of new stuff.

--- src/server/controllers/chime.js
+++ src/server/controllers/chime.js
@@ -1,23 +1,10 @@
-const brain = require('brain.js');
-const api = require('./controllers.js');
-const util = require('util')
-const { dialogflow, Image, Table, Carousel } = require('actions-on-google');
+const Feedback = require('../models/feedback');
 
 module.exports = {
-    chime: async(req, res)=>{
-        console.log(`post/${util.inspect(req,false,null)}`);
-        // const request = req.body.result;
-        // const intent = req.body.arguments.rawText;
-        //
-        // switch(intent){
-        //     case 'hello':
-        //         console.log(req);
-        //         api.hello(request, res);
-        // }
-        return res;
-    }
-
-
+    feedback: async(request)=>{
+        await Feedback.save(request);
+        console.log('new feedback made');
+    },
 
 };
 
