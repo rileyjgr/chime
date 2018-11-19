eecb28cfffe8ea1fe8a91591d0b3097c07d715ed
Riley Griffin <rigriffin241@gmail.com>
2018-11-19 17:04:59 -0500

    google calendar almost working

--- src/server/controllers/chime-hook.js
+++ src/server/controllers/chime-hook.js
@@ -6,29 +6,70 @@ const Feedback = require('../models/feedback');
 module.exports = {
     
     hello: async(agent)=>{
+        const req = JSON.stringify(agent.body);
+        console.log('agent: ' + agent);
         agent.add('Hello.')
     },
     // Create Event Intent
     event: async(agent)=>{
-        // this is how you get parameters
-        console.log(agent.parameters);
-        
-        const user = agent.userId;
-        const foundUser = await User.findOne({user});
 
-        switch(foundUser){
-            case true:
-                console.log(foundUser);
-                agent.add('user found, events hit')
-                break;
-            case false:
-                // const token = await googleapis.getToken(user);
-
-                // const newUser = new User({user, token});
-                // await newUser.save();
-                agent.add('no user found, events hit')
+        const convertParametersDate = (date, time) =>{
+            return new Date(Date.parse(date.split('T')[0] + 'T' + time.split('T')[1].split('-')[0] + timeZoneOffset));
         }
+        const addHours = (dateObj, hoursToAdd)=>{
+            return new Date(new Date(dateObj).setHours(dateObj.getHours() + hoursToAdd));
+        }        
         
+        const getLocaleTimeString = (dateObj)=>{
+            return dateObj.toLocaleTimeString('en-US', { hour: 'numeric', hour12: true, timeZone: timeZone });
+        }
+
+        const getLocaleDateString= (dateObj)=> {
+            return dateObj.toLocaleDateString('en-US', { weekday: 'long', month: 'long', day: 'numeric', timeZone: timeZone });
+        }
+
+        const createCalendarEvent = (dateTimeStart, dateTimeEnd) => {
+            return new Promise((resolve, reject) => {
+                calendar.events.list({  // List all events in the specified time period
+                  auth: serviceAccountAuth,
+                  calendarId: calendarId,
+                  timeMin: dateTimeStart.toISOString(),
+                  timeMax: dateTimeEnd.toISOString()
+                }, (err, calendarResponse) => {
+                  // Check if there exists any event on the calendar given the specified the time period
+                  if (err || calendarResponse.data.items.length > 0) {
+                    reject(err || new Error('Requested time conflicts with another appointment'));
+                  } else {
+                    // Create an event for the requested time period
+                    calendar.events.insert(
+                        {   
+                            auth: serviceAccountAuth, 
+                            calendarId: calendarId,
+                            resource: {summary: 'Bike Appointment',
+                            start: {dateTime: dateTimeStart},
+                            end: {dateTime: dateTimeEnd}
+                        }
+                    }, (err, event) => {
+                      err ? reject(err) : resolve(event);
+                    }
+                    );
+                  }
+                });
+              });
+            }
+        const dateTimeStart = convertParametersDate(agent.parameters.date, agent.parameters.time);
+        const dateTimeEnd = addHours(dateTimeStart, 1);
+        const appointmentTimeString = getLocaleTimeString(dateTimeStart);
+        const appointmentDateString = getLocaleDateString(dateTimeStart);
+
+
+
+        // Check the availability of the time, and make an appointment if there is time on the calendar
+        return createCalendarEvent(dateTimeStart, dateTimeEnd).then(() => {
+          agent.add(`Got it. I have your appointment scheduled on ${appointmentDateString} at ${appointmentTimeString}. See you soon. Good-bye.`);
+        }).catch(() => {
+          agent.add(`Sorry, we're booked on ${appointmentDateString} at ${appointmentTimeString}. Is there anything else I can do for you?`);
+        });
     },
 
     // Get Calendar Info
--- src/server/controllers/dialogflowcontroller.js
+++ src/server/controllers/dialogflowcontroller.js
@@ -1,16 +1,42 @@
 'use strict';
 
 const functions = require('firebase-functions');
+const {google} = require('googleapis');
 const {WebhookClient} = require('dialogflow-fulfillment');
 const chime = require('./chime-hook');
 
+require('dotenv');
+// Enter your calendar ID and service account JSON below.
+const calendarId = process.env.CAL_ID; // Example: 6ujc6j6rgfk02cp02vg6h38cs0@group.calendar.google.com
+const serviceAccount = {  
+"type": "service_account",
+"project_id": process.env.PROJECT_ID,
+"private_key_id": process.env.PRIVATE_KEY_ID,
+"private_key": process.env.PRIVATE_KEY,
+"client_email": process.env.CLIENT_EMAIL,
+"client_id": process.env.CLIENT_ID,
+"auth_uri": "https://accounts.google.com/o/oauth2/auth",
+"token_uri": "https://oauth2.googleapis.com/token",
+"auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+"client_x509_cert_url": process.env.CLIENT_x509_CERT_URL
+}; // The JSON object looks like: { "type": "service_account", ... }
+
+// Set up Google Calendar service account credentials
+const serviceAccountAuth = new google.auth.JWT({
+  email: serviceAccount.client_email,
+  key: serviceAccount.private_key,
+  scopes: 'https://www.googleapis.com/auth/calendar'
+});
+
+const calendar = google.calendar('v3');
+
+const timeZone = 'America/Los_Angeles';  // Change it to your time zone
+const timeZoneOffset = '-07:00'; 
 exports.dialogflowFirebaseFulFillment = functions.https.onRequest((request, response)=>{
     const agent = new WebhookClient({request, response});
-
     console.log('Dialogflow Request body: ' + JSON.stringify(request.body));
 
     let intentMap = new Map();
-
     intentMap.set('Hello', chime.hello);
     intentMap.set('Feedback', chime.feedback);
     intentMap.set('Event',chime.event);
@@ -18,5 +44,4 @@ exports.dialogflowFirebaseFulFillment = functions.https.onRequest((request, resp
     intentMap.set('weather', chime.weather);
     
     agent.handleRequest(intentMap);
-
 });
