09fb9519e0283908e1a0af28d127d7ff0bdd13eb
Riley Griffin <rigriffin241@gmail.com>
2018-11-19 20:57:15 -0500

    Calendar endpoint working'

--- src/server/controllers/chime-hook.js
+++ src/server/controllers/chime-hook.js
@@ -1,48 +1,51 @@
-const Event = require('../models/event');
-const User = require('../models/user');
-const Feedback = require('../models/feedback');
-const {google} = require('googleapis');
-const timeZone = 'America/New_York';
-const timeZoneOffset = '-05:00'; 
+const Event = require("../models/event");
+const User = require("../models/user");
+const Feedback = require("../models/feedback");
+const {google} = require("googleapis");
+const timeZone = "America/New_York";
+const timeZoneOffset = "-05:00"; 
 
-const serviceAccount = {  
-    'type': 'service_account',
-    'project_id': process.env.PROJECT_ID,
-    'private_key_id': process.env.PRIVATE_KEY_ID,
-    'private_key': process.env.PRIVATE_KEY,
-    'client_email': process.env.CLIENT_EMAIL,
-    'client_id': process.env.CLIENT_ID,
-    'auth_uri': 'https://accounts.google.com/o/oauth2/auth',
-    'token_uri': 'https://oauth2.googleapis.com/token',
-    'auth_provider_x509_cert_url': 'https://www.googleapis.com/oauth2/v1/certs',
-    'client_x509_cert_url': process.env.CLIENT_x509_CERT_URL
-}; 
-
-// Set up Google Calendar service account credentials
-const serviceAccountAuth = new google.auth.JWT({
-    email: serviceAccount.client_email,
-    key: serviceAccount.private_key,
-    scopes: 'https://www.googleapis.com/auth/calendar'
-});
-
-const calendar = google.calendar('v3');
-
-require('dotenv').config();
+require("dotenv").config();
 
 module.exports = {
     
     hello: async(agent)=>{
         const req = JSON.stringify(agent.body);
-        console.log('agent: ' + agent);
-        agent.add('Hello.')
+        console.log("agent: " + agent);
+        agent.add("Hello.")
     },
     // Create Event Intent
     event: async(agent)=>{
+        const serviceAccount = {  
+            "type": "service_account",
+            "project_id": process.env.PROJECT_ID,
+            "private_key_id": process.env.PRIVATE_KEY_ID,
+            "private_key": process.env.PRIVATE_KEY,
+            "client_email": process.env.CLIENT_EMAIL,
+            "client_id": process.env.CLIENT_ID,
+            "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+            "token_uri": "https://oauth2.googleapis.com/token",
+            "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+            "client_x509_cert_url": process.env.CLIENT_x509_CERT_URL
+        }; 
+
+        console.log(serviceAccount);
+
+        
+        // Set up Google Calendar service account credentials
+        const serviceAccountAuth = new google.auth.JWT({
+            email: serviceAccount.client_email,
+            key: serviceAccount.private_key,
+            scopes: "https://www.googleapis.com/auth/calendar"
+        });
+        
+        const calendar = google.calendar("v3");
+
         const date = agent.parameters.date;
         const time = agent.parameters.time;
         
         const convertParametersDate = (date, time) =>{
-            return new Date(Date.parse(date + 'T' + time + timeZoneOffset));
+            return new Date(Date.parse(date + "T" + time + timeZoneOffset));
         }
 
         const addHours = (dateObj, hoursToAdd)=>{
@@ -50,19 +53,18 @@ module.exports = {
         }        
         
         const getLocaleTimeString = (dateObj)=>{
-            return dateObj.toLocaleTimeString('en-US', { hour: 'numeric', hour12: true, timeZone: timeZone });
+            return dateObj.toLocaleTimeString("en-US", { hour: "numeric", hour12: true, timeZone: timeZone });
         }
 
         const getLocaleDateString= (dateObj)=> {
-            return dateObj.toLocaleDateString('en-US', { weekday: 'long', month: 'long', day: 'numeric', timeZone: timeZone });
+            return dateObj.toLocaleDateString("en-US", { weekday: "long", month: "long", day: "numeric", timeZone: timeZone });
         }
-
+        const appointmentDuration = 1;
         const dateTimeStart = convertParametersDate(date, time);
         const dateTimeEnd = addHours(dateTimeStart, 1);
         const appointmentTimeString = getLocaleTimeString(dateTimeStart);
         const appointmentDateString = getLocaleDateString(dateTimeStart);
         const calendarId = process.env.CAL_ID;
-        console.log(calendar);
 
         const createCalendarEvent = (dateTimeStart, dateTimeEnd) => {
             console.log(dateTimeStart);
@@ -76,7 +78,7 @@ module.exports = {
                     console.log(calendarResponse);
                   // Check if there exists any event on the calendar given the specified the time period
                   if (err || calendarResponse.data.items.length > 0) {
-                    reject(err || new Error('Requested time conflicts with another Meeting'));
+                    reject(err || new Error("Requested time conflicts with another Meeting"));
                     console.log(err);
                   } else {
                     // Create an event for the requested time period
@@ -84,7 +86,7 @@ module.exports = {
                         {   
                             auth: serviceAccountAuth, 
                             calendarId: calendarId,
-                            resource: {summary: 'Meeting',
+                            resource: {summary: "Meeting",
                             start: {dateTime: dateTimeStart},
                             end: {dateTime: dateTimeEnd}
                         }
@@ -101,13 +103,13 @@ module.exports = {
         return createCalendarEvent(dateTimeStart, dateTimeEnd).then(() => {
           agent.add(`Got it. I have your Meeting scheduled on ${appointmentDateString} at ${appointmentTimeString}.`);
         }).catch(() => {
-          agent.add(`Sorry, we're booked on ${appointmentDateString} at ${appointmentTimeString}. Is there anything else I can do for you?`);
+          agent.add(`Sorry, we"re booked on ${appointmentDateString} at ${appointmentTimeString}. Is there anything else I can do for you?`);
         });
     },
 
     // Get Calendar Info
     getInfo: async(agent)=>{
-        agent.add('get info hit');
+        agent.add("get info hit");
     },
     
     feedback: async(agent)=>{
@@ -115,14 +117,14 @@ module.exports = {
         const newFeedback = new Feedback({message: userFeedback});
         await newFeedback.save();
 
-        agent.add('Thank you for your feedback, your feedback request was:' + newFeedback + 'this was sent to your supervisor anonymously');
+        agent.add("Thank you for your feedback, your feedback request was:" + newFeedback + "this was sent to your supervisor anonymously");
     },
     
     weather: async(agent)=>{
         console.log(agent.parameters);
         const {name, city, etc} = agent.parameters;
 
-        const weather = '';
+        const weather = "";
         agent.add(weather);
     }
 
