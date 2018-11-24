9438976399f4eef14fde18d8b2260f04af31907e
Riley Griffin <rigriffin241@gmail.com>
2018-11-24 11:19:33 -0500

    fixed webpack issue

--- src/client/Components/Login.jsx
+++ src/client/Components/Login.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import { Button, FormGroup, FormControl, ControlLabel } from "react-bootstrap";
 import Navbar from './Navbar';
-import '../App.css';
+// import '../App.css';
 
 export default class Login extends Component {
   constructor(props) {
