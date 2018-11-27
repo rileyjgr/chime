a824a3f0c96fd8512f5aa77af2f389959eba4e39
REstrella99 <RafaelEstrella99@gmail.com>
2018-11-26 20:59:10 -0500

    Feedback

--- src/client/Components/Tasks.jsx
+++ src/client/Components/Tasks.jsx
@@ -2,56 +2,40 @@ import React, { Component } from "react";
 import { Tooltip, OverlayTrigger } from "react-bootstrap";
 import Checkbox from "./CustomCheckbox.jsx";
 import Button from "./CustomButton.jsx";
+import axios from 'axios';
 
-export class Tasks extends Component {
-  handleCheckbox = event => {
-    const target = event.target;
-    console.log(event.target);
-    this.setState({
-      [target.name]: target.checked
-    });
-  };
-  render() {
-    const edit = <Tooltip id="edit_tooltip">Edit Task</Tooltip>;
-    const remove = <Tooltip id="remove_tooltip">Remove</Tooltip>;
-    const tasks_title = [
-      'Sign contract for "What are conference organizers afraid of?"',
-      "Lines From Great Russian Literature? Or E-mails From My Boss?",
-      "Flooded: One year later, assessing what was lost and what was found when a ravaging rain swept through metro Detroi",
-      "Create 4 Invisible User Experiences you Never Knew About",
-      'Read "Following makes Medium better"',
-      "Unfollow 5 enemies from twitter"
-    ];
-    var tasks = [];
-    var number;
-    for (var i = 0; i < tasks_title.length; i++) {
-      number = "checkbox" + i;
-      tasks.push(
-        <tr key={i}>
-          <td>
-            <Checkbox
-              number={number}
-              isChecked={i === 1 || i === 2 ? true : false}
-            />
-          </td>
-          <td>{tasks_title[i]}</td>
-          <td className="td-actions text-right">
-            <OverlayTrigger placement="top" overlay={edit}>
-              <Button bsStyle="info" simple type="button" bsSize="xs">
-                <i className="fa fa-edit" />
-              </Button>
-            </OverlayTrigger>
 
-            <OverlayTrigger placement="top" overlay={remove}>
-              <Button bsStyle="danger" simple type="button" bsSize="xs">
-                <i className="fa fa-times" />
-              </Button>
-            </OverlayTrigger>
-          </td>
-        </tr>
-      );
+class Tasks extends Component {
+  constructor() {
+    super();
+    this.state = {
+      feedback: [{ "_id": "5bea1d812f9ca0088cb3d335", "message": "i dont like jano", "__v": 0 }]
     }
-    return <tbody>{tasks}</tbody>;
+  }
+
+  // componentDidMount() {
+  //   axios.get(`/api/feedback`)
+  //     .then(async (res) => {
+  //       const feedbackData = res.data;
+  //       let { message } = '';
+  //       const feedback = { message };
+  //       let feedbackArray = [];
+  //       for (let key in feedbackData) {
+  //         message = feedbackData[key].message;
+
+  //         const newFeedback = { message };
+  //         feedbackArray.push(newFeedback);
+  //       }
+  //       this.setState({ feedback: feedbackArray });
+  //     }).catch(err => {
+  //       this.setState({ feedback: err });
+  //     })
+  // }
+  render() {
+    <div id="feedback">
+      {this.state.feedback.map(feeds => <div><h5>{feedback.message}</h5>></div>)}
+    </div>
+
   }
 }
 
