f18a8fb9cc931262096da75b73391a5620b47892
Christiantav <chrisadrianotav@gmail.com>
2018-11-28 01:17:33 -0500

    styling fixes

--- src/client/Components/HomePage.jsx
+++ src/client/Components/HomePage.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Button, FormGroup, FieldGroup, PageHeader, FormControl, ControlLabel, Jumbotron, Nav, NavItem, Row } from "react-bootstrap";
+import { Button, FormGroup, FieldGroup, PageHeader, FormControl, Col, ControlLabel, Image, Jumbotron, Nav, NavItem, Row } from "react-bootstrap";
 import HeatMap from './Map';
 import Navbar from './Navbar';
 import '../App.css';
@@ -14,11 +14,13 @@ class HomePage extends Component {
                             <Navbar/>
                             <Jumbotron>
                                 <div>
-                                    <span className="text-center"><h1>Welcome to Chime</h1>
-                                    <br/>
-                                        <span className="text-center"><h3>Giving your workforce a voice</h3>
-                                        </span>
+                                    <span className="text-center">
+                                        <h1>Welcome to Chime</h1>
                                     </span> 
+                                    <div className="text-center inlined">
+                                        <Image src="http://icons.iconarchive.com/icons/custom-icon-design/pretty-office-10/128/Bell-icon.png" rounded />
+                                        <h3>Giving your workforce a voice</h3>
+                                    </div>
                                 </div>
                             </Jumbotron>
                         </div>
@@ -28,13 +30,13 @@ class HomePage extends Component {
                 <div className="wrapper2 tabled">
                     <div className="stage" id="about">
                         <div className="middled">
-                            <Jumbotron className='titles'><span className="text-center"><h1>About Us</h1></span></Jumbotron>
-                            <ul>
-                                <li>A group of students at the Columbia x Trilogy coding bootcamp had a dream</li>
-                                <li>Companies were out of control, disorganized, and not hearing from their workforce</li>
-                                <li>We sought out to give those workforces a voice</li>
-                                <li>So we wanted to give all employees the ability to chime in with management</li>
-                            </ul>
+                            <Jumbotron className='titles'>
+                                <span className="text-center"><h1>About Us</h1></span>
+                                <p className="text-center">A group of coding bootcamp students @ Columbia University wanted to create a chatbot integration to serve company employees with a variety of task automation. We also sought to make an interface to provide anonymous feedback, departmental engagement, and a company calendar for HR departments on our web app.</p>
+                                <p className="text-center">
+                                    <Button bsStyle="primary" href="https://github.com/rileyjgr/chime">Github!</Button>
+                                </p>
+                            </Jumbotron>
                         </div>
                     </div>
                 </div>
@@ -42,10 +44,12 @@ class HomePage extends Component {
                 <div className="wrapper2 tabled">
                     <div className="stage" id="skills">
                         <div className="middled">
-                            <Jumbotron className='titles'><span className="text-center"><h1>Chime Skills</h1></span></Jumbotron>
+                            <Jumbotron className='titles'><span className="text-center">
+                                <h1>Chime Skills</h1></span>
+                            </Jumbotron>
                             <ul>
                                 <li>I can set a calendar for your company</li>
-                                <li>Want to hear about the weather for your work-week?</li>
+                                <li>Want to hear about the weather during your work-week?</li>
                                 <li>Lets submit some feedback to HR!!</li>
                                 <li>Track employee engagement while you're at it</li>
                             </ul>
