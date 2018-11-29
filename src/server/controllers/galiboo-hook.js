84e8819202a632cd5f6c7865e593a4f121de9811
Riley Griffin <rigriffin241@gmail.com>
2018-11-29 15:24:47 -0500

    have spotify urls generating

--- src/server/controllers/galiboo-hook.js
+++ src/server/controllers/galiboo-hook.js
@@ -3,35 +3,40 @@ require('dotenv').config();
 
 module.exports = {
     music: async(agent)=>{
-        console.log(agent.parameters.Queries);
+        console.log(agent.parameters.queries);
+        const potentionalQuerys = [agent.parameters];
+        console.log(potentionalQuerys);
+        const getQuerys = (potentionalQuerys) => {
 
+
+
+        };
         // const queries = agent.pareters.Queries;
-
-        const headers = {
-            token: process.env.GALIBOO_ACCESS_TOKEN,
-            threshold: 0.6,
-            q: 'jazz',
-            count: 5,
-            page: 1
-        }
         const token = '?token=' + process.env.GALIBOO_ACCESS_TOKEN;
-        const query = '&q=jazz';
+        const query = '&q=' + potentionalQuerys;
 
+        let spotify_ids_array = [];
         let songsArray = [];
+        // call to galiboo to get spotify ids based on user inputs 
         await axios.get('http://secure.galiboo.com/api/discover/tracks/smart_search/'+ token + query)
         .then((response)=>{
             const songData = response.data.results;
             for(let key in songData){
-                let url = songData[key].audio_url;
-                console.log(url);
-                songsArray.push(url);
+                let spotify_id = songData[key].external_ids.spotify;
+                spotify_ids_array.push(spotify_id);
             }
         })
         .catch((error)=>{
             console.log(error);
         });
 
-        console.log(songsArray);
-        return agent.add('I have a list of songs for you,' + songsArray.join('\n'));
+        // call to spotify api to get data based on ids that galiboo gives us.
+        await spotify_ids_array.forEach((entry)=>{
+            const spotify_url = 'https://open.spotify.com/track/' + entry;
+            songsArray.push(spotify_url);
+        });
+
+
+        return agent.add('Okay I have generated some spotify links for you,' + '\n' + songsArray.join('\n'));
     }
 };
